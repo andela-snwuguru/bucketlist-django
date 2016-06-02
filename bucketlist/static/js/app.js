6a7df86a788ab9b2cc5709d8142693eff645709a
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-06-02 17:10:44 +0100

    [Feature] Implemented CRUD for bucket list item in front end

--- bucketlist/static/js/app.js
+++ bucketlist/static/js/app.js
@@ -1,4 +1,4 @@
-angular.module('Bucketlist', ['BucketlistControllers', 'BucketlistServices', 'ngMaterial', 'ngResource'])
+angular.module('Bucketlist', ['bucketlist.controllers', 'bucketlist.services', 'ngMaterial', 'ngResource'])
     .config(function($interpolateProvider, $httpProvider) {
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
@@ -6,5 +6,6 @@ angular.module('Bucketlist', ['BucketlistControllers', 'BucketlistServices', 'ng
         $interpolateProvider.endSymbol('}]}');
     })
     .constant('CONFIG', {
-        apiUrl: 'http://127.0.0.1:8000/api/v1'
+        apiUrl: 'http://127.0.0.1:8000/api/v1',
+        loadItemsEvent: 'active-bucketlist-change'
     });
