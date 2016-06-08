e7e1d25940bf2038a591f60e8aaeb239b18f9bdd
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-06-08 20:14:18 +0100

    [Chores] Modified requirements.txt

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@f*bztzc-o%4zoas@34fwdss)^+98v+b96ea_w!(+uitaw&#_e'
+SECRET_KEY =  '@f*bztzc-o%4zoas@34fwdss)^+98v+b96ea_w!(+uitaw&#_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 import dj_database_url
